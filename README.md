2b50ff15adb83487f0ba1f6871b0a812d6a8b8c8
Maxim Butenko <maxbuten97@yandex.ru>
2023-12-23 18:08:16 +0700

    add prettier, Redux toolkit, Material UI

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 npm run / npm run start
 
 Сборка:
-npm run build:prod / npm run build:dev  | прод и девелоп
+npm run build:prod / npm run build:dev | прод и девелоп
 
+React 18 Typescript
+
+UI-библиотека: MaterialUI
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,38 +1,40 @@
-import { useState } from 'react';
-import s from './App.module.scss';
-import { Link, Outlet } from 'react-router-dom';
-import './normalize.scss';
+import { useState } from "react";
+import s from "./App.module.scss";
+import { Link, Outlet } from "react-router-dom";
+import "./normalize.scss";
 
 export const App = () => {
-	const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState<number>(0);
 
-	const increment = () => setCount(prev => prev + 1)
-	const decrement = () => setCount(prev => prev - 1)
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
 
-	// if(__PLATFORM__ === 'desktop') {
-	// 	return <div>DESKTOP PLATFORM</div>
-	// }
-	// if(__PLATFORM__ === 'mobile') {
-	// 	return <div>MOBILE PLATFORM</div>
-	// }
+  // if(__PLATFORM__ === 'desktop') {
+  // 	return <div>DESKTOP PLATFORM</div>
+  // }
+  // if(__PLATFORM__ === 'mobile') {
+  // 	return <div>MOBILE PLATFORM</div>
+  // }
 
-	// if(__ENV__ === 'development') {
-	// 	addDevTools()
-	// }
+  // if(__ENV__ === 'development') {
+  // 	addDevTools()
+  // }
 
-
-	return (
-		<div>
-			<div className={s.links}>
-			<Link to={'/'}>Главная</Link>
-			<Link to={'/about'}>О нас</Link>
-			<Link to={'/shop'}>Магазин</Link>
-			</div>
-			<div className={s.value}>{count}</div>
-			<button className={s.button} onClick={decrement}>-</button>
-			<button className={s.button} onClick={increment}>+</button>
-			<Outlet />
-		</div>
-		
-	);
+  return (
+    <div>
+      <div className={s.links}>
+        <Link to={"/"}>Главная</Link>
+        <Link to={"/about"}>О нас</Link>
+        <Link to={"/shop"}>Магазин</Link>
+      </div>
+      <div className={s.value}>{count}</div>
+      <button className={s.button} onClick={decrement}>
+        -
+      </button>
+      <button className={s.button} onClick={increment}>
+        +
+      </button>
+      <Outlet />
+    </div>
+  );
 };
