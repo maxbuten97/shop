687930486dba1418bf696d7b72ddcdce9d135db6
Maxim Butenko <maxbuten97@yandex.ru>
2024-01-08 10:56:44 +0700

    создал кнопку удаления

--- src/components/CartDropdown/CartDropdown.tsx
+++ src/components/CartDropdown/CartDropdown.tsx
@@ -4,11 +4,13 @@ import { BsCart, BsX } from "react-icons/bs";
 import { useTypedSelector } from "@/Hooks/useTypedSelector";
 import { useOutside } from "@/Hooks/useOutside";
 import s from "./CartDropdown.module.scss";
+import { useActions } from "@/Hooks/useActions";
+import { Button } from "@mui/material";
 
 const CartDropdown: FC = () => {
   const { ref, isShow, setIsShow } = useOutside(false);
   const { cart } = useTypedSelector((state) => state);
-
+  const { removeItem } = useActions();
   return (
     <>
       <button className={s.btn_dropdown} onClick={() => setIsShow(!isShow)}>
@@ -19,7 +21,7 @@ const CartDropdown: FC = () => {
         <div className={s.modal} style={{ minHeight: "45%" }} ref={ref}>
           {cart.length ? (
             <>
-              {cart.map((product: any) => (
+              {cart.map((product) => (
                 <div key={`Cart item: ${product.id}`} className={s.card_wrap}>
                   <div className={s.card}>
                     <div className={s.card_img}>
@@ -36,6 +38,9 @@ const CartDropdown: FC = () => {
                       <div className={s.card_price}>${product.price}</div>
                     </div>
                   </div>
+                  <Button onClick={() => removeItem({ id: product.id })}>
+                    Удалить
+                  </Button>
                 </div>
               ))}
               <PlaceOrder />
